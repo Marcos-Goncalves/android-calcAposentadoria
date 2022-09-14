7d11b0c240c9e317b670f3008ba65ea2b5722006
Marcos-Goncalves <kirottow17@gmail.com>
2022-09-13 23:19:03 -0300

    feat: implementando a MainActivity e criando CalcularActivity

---
+++ app/src/main/java/com/app/calcaposentadoria/CalcularActivity.java
@@ -0,0 +1,40 @@
+package com.app.calcaposentadoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class CalcularActivity extends AppCompatActivity {
+    String nome;
+    boolean sexo;
+    int idade, contribuicao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcular);
+
+        nome = getIntent().getStringExtra("nome");
+        sexo = getIntent().getBooleanExtra("F", false);
+        idade = getIntent().getIntExtra("idade", 0);
+        contribuicao = getIntent().getIntExtra("contribuicao", 0);
+
+        ((TextView)findViewById(R.id.idNome).setText("nome"));
+        ((TextView)findViewById(R.id.idIdade).setText("idade"));
+    }
+
+    public void aposentar(View view){
+        if (sexo == true && idade >= 62 && contribuicao >= 15 || sexo == false && idade >= 65 && contribuicao >= 20){
+            ((TextView)findViewById(R.id.idMensagem).setText("Pode se aposentar!"));
+        }else {
+            ((TextView)findViewById(R.id.idMensagem).setText("Ainda não pode se aposentar!"));
+            if(sexo == true) {
+                ((TextView) findViewById(R.id.idCalculo).setText("Ainda precisa trabalhar " + (15-contribuicao)));
+            } else {
+                ((TextView) findViewById(R.id.idCalculo).setText("Ainda precisa trabalhar " + (20-contribuicao)));
+            }
+        }
+    }
+}
--- app/src/main/java/com/app/calcaposentadoria/MainActivity.java
+++ app/src/main/java/com/app/calcaposentadoria/MainActivity.java
@@ -2,7 +2,11 @@ package com.app.calcaposentadoria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calcular (View view){
+        Intent intent = new Intent(getApplicationContext(), CalcularActivity.class);
+        intent.putExtra("nome", ((EditText)findViewById(R.id.idNome)).getText().toString());
+        intent.putExtra("F", ((RadioButton)findViewById(R.id.btF)).isSelected());
+        intent.putExtra("idade", ((EditText)findViewById(R.id.idIdade)).getText().toString());
+        intent.putExtra("contribuicao", ((EditText)findViewById(R.id.idContribuicao)).getText().toString());
     }
 }
